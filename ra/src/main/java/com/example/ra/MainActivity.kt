2efe975cb513e2ec70629ff3a12a9982c67f8e66
Ivan <galic.ivan123321@gmail.com>
2024-09-11 15:36:09 +0200

    Osnovni dio

--- ra/src/main/java/com/example/ra/MainActivity.kt
+++ ra/src/main/java/com/example/ra/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.ra
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ra.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        binding.mainUpload.setOnClickListener{
+            val intent = Intent(this@MainActivity, UploadActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
--- ra/src/main/java/com/example/ra/UploadActivity.kt
+++ ra/src/main/java/com/example/ra/UploadActivity.kt
@@ -1,20 +1,56 @@
 package com.example.ra
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ra.databinding.ActivityMainBinding
+import com.example.ra.databinding.ActivityUploadBinding
+import com.example.ra.databinding.ActivityUploadBinding.inflate
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.lang.ref.Reference
 
 class UploadActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUploadBinding
+    private lateinit var databaseReference: DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_upload)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saveButton.setOnClickListener{
+            val ownerName = binding.uploadOwnerName.text.toString()
+            val vehicleBrand = binding.uploadVehicleBrand.text.toString()
+            val vehicleRTO = binding.uploadVehicleRTO.text.toString()
+            val vehicleNumber = binding.uploadVehicleNumber.text.toString()
+
+
+            databaseReference = FirebaseDatabase.getInstance().getReference("Vehicle Information")
+            val vehicleData = VehicleData(ownerName, vehicleBrand,vehicleRTO, vehicleNumber)
+            databaseReference.child(vehicleNumber).setValue(vehicleData).addOnSuccessListener {
+                binding.uploadOwnerName.text.clear()
+                binding.uploadVehicleBrand.text.clear()
+                binding.uploadVehicleRTO.text.clear()
+                binding.uploadVehicleNumber.text.clear()
+
+                Toast.makeText(this, "Spremljeno!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@UploadActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+
+            }.addOnFailureListener{
+                Toast.makeText(this, "Nije spremljeno!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
         }
     }
-}
