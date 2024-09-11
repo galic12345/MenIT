637e038761cfdd358e1057dc45422e0756e21dc4
Ivan <galic.ivan123321@gmail.com>
2024-09-11 18:10:16 +0200

    Kraj

--- ra/src/main/java/com/example/ra/DeleteActivity.kt
+++ ra/src/main/java/com/example/ra/DeleteActivity.kt
@@ -1,16 +1,43 @@
 package com.example.ra
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ra.databinding.ActivityDeleteBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class DeleteActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityDeleteBinding
+    private lateinit var databaseReference: DatabaseReference
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDeleteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.deleteButton.setOnClickListener{
+            val vehicleNumber = binding.deleteVehicleNumber.text.toString()
+            if(vehicleNumber.isNotEmpty()){
+                deleteData(vehicleNumber)
+            }else{
+                Toast.makeText(this, "Unesite broj auta!", Toast.LENGTH_SHORT).show()
+            }
+        }
 
-        setContentView(R.layout.activity_delete)
-
+    }
+    
+    private fun deleteData(vehicleNumber: String){
+        databaseReference = FirebaseDatabase.getInstance().getReference("Vehicle Information")
+        databaseReference.child(vehicleNumber).removeValue().addOnSuccessListener { 
+            binding.deleteVehicleNumber.text.clear()
+            Toast.makeText(this,  "Obrisano!", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener{
+            Toast.makeText(this,  "Nije moguće obrisati!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
